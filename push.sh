1dd001b4e8f9c2011a1f7879ff5c8eb3646b03e9
gstrainovic <g.strainovic@gmail.com>
2021-12-22 21:12:07 +0100

    u push.sh

--- push.sh
+++ push.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
+echo '####################################################'
 echo 'git config'
+echo '####################################################'
 git config --global user.email "g.strainovic@gmail.com"
 git config --global user.name "gstrainovic"
 
+echo '####################################################'
+echo 'git checkout master'
+echo '####################################################'
+git checkout master
 
-echo 'git status'
-git status
-
+echo '####################################################'
 echo 'git add example/export.html'
+echo '####################################################'
 git add example/export.html
 
+echo '####################################################'
 echo 'git commit: push export.html from azure pipeline'
+echo '####################################################'
 git commit -m 'push export.html from azure pipeline'
 
+echo '####################################################'
 echo 'git push'
+echo '####################################################'
 git push origin
 
+echo '####################################################'
 echo 'git status'
+echo '####################################################'
 git status
