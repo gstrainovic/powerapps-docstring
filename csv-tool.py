f14df9227256b2dffcd3af4677c7bca3faae7f5c
gstrainovic <g.strainovic@gmail.com>
2022-01-19 03:15:20 +0100

    csv, read,replace,add

--- csv-tool.py
+++ csv-tool.py
@@ -1,90 +1,109 @@
 import glob
 import sys
-import yaml
 import csv
+
 
 def main(argv):
     pa_src_path = "example/src/*/Src/*.yaml"
     filenames = glob.glob(pa_src_path)
 
-    def get_screen_content(screen_name):
-        screen_path = screen_name 
-        screen_content = {}
+    def finder(screen_content, asScreen, key):
+        key = key + ':'
+        asFound = False
+        ar = []
+        for line in screen_content:
+            if asScreen in line:
+                asFound = True
+            elif asFound:
+                if key in line:
+                    start_index = line.find(key)
+                    old_value = line[start_index + len(key):]
+                    if start_index != -1:
+                        return old_value.strip(), 'screen,key,value found'
+                elif line == '\n':
+                    asFound = False
+                    return '', 'screen found, key not found'
 
-        with open(screen_path, "r", encoding='utf8') as file:
-            screen_content = yaml.load(file, Loader=yaml.BaseLoader)
+    def replaceOrAdd(screen_content, asScreen, key, new_value, status):
+        key = key + ':'
+        asFound = False
+        ar = []
+        for line in screen_content:
+            if asScreen in line and 'As' in line:
+                spaces_start_count = len(line) - len(line.lstrip()) + 4
+                spaces_start = spaces_start_count * ' '
+                asFound = True
+            elif asFound:
+                if key in line and status=='replace':  # replace
+                    start_index = line.find(key)
+                    old_value = line[start_index + len(key):].strip()
+                    line = line.replace(old_value, new_value)
+                    status='replaced'
+                elif line == '\n' and status=='add':  # add line
+                    ar.append(spaces_start +
+                              key + " " + new_value)
+                    ar.append('\n')
+                    asFound = False
 
-        return screen_content
+            ar.append(line)
 
-
-    def find_keys(d, target):
-        for k, v in d.items():
-            if target in k:
-                return [k]
-            if isinstance(v, dict):
-                p = find_keys(v, target)
-                if p:
-                    return [k] + p
-
-
-
-    def find_value_from_key(d, target):
-        for k, v in d.items():
-            if target in k:
-                yield v
-            if isinstance(v, dict):
-                for i in find_value_from_key(v, target):
-                    yield i
+        return ar
 
     output_csv = open('output.csv', 'w', newline='')
     csv_writer = csv.writer(output_csv, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     csv_writer.writerow(['as', 'key', 'value', 'status'])
 
-
-    # opening the file using "with"
-    # statement
     with open('input.csv') as input_csv:
         for line in csv.DictReader(input_csv):
             asScreen = line['as']
             key = line['key']
-            value = line['value']
+            value_from_csv = line['value']
 
-            for file in filenames:
-                screen_content = get_screen_content(file)
+            for filepath in filenames:
+                with open(filepath, "r", encoding='utf8') as file:
+                    screen_content = file.readlines()
 
-                arr = []
-                for val in find_value_from_key(screen_content, asScreen):
-                    arr.append(val)
+                find_result = finder(screen_content, asScreen, key)
 
-                if arr: # asScreen found
-                    keys = find_keys(screen_content, key)
-                    if keys and not value:
-                        nvalue = arr[0][key]
-                        mytuple = asScreen, key, nvalue, 'value read ok'
-                        print(mytuple)
-                        csv_writer.writerow(mytuple)
-                    if keys and value:
-                        arr[0][key] = "=helloWorld"
-                        #kk = ['CameraScreen As screen']['CameraNavBar As group']['AppLogo3 As image']['ImagePosition']
-                        
-                        # don't work
-                        ## screen_content.update(arr[0][key])
-                        
-                        # with open(r'new.yaml', 'w') as write_yaml:
-                        #     documents = yaml.dump(screen_content, write_yaml)
-                        mytuple = asScreen, key, value, 'value replace ok'
-                        print(mytuple)
-                        csv_writer.writerow(mytuple)
-                    if not keys and value:
-                        mytuple = asScreen, key, value, 'add key and value ok'
-                        print(mytuple)
-                        csv_writer.writerow(mytuple)
+                if find_result:
+                    value_in_yaml, status = find_result
 
+                    # find value
+                    if not value_from_csv and status == 'screen,key,value found':
+                        mytuple = asScreen, key, value_in_yaml, 'value read ok'
+
+
+                    # replace
+                    elif value_from_csv and status == 'screen,key,value found':
+                        new_yaml = replaceOrAdd(
+                            screen_content, asScreen, key, value_from_csv, 'replace')
+
+                        with open(r'new.yaml', 'w') as write_yaml:
+                            write_yaml.writelines(new_yaml)
+
+                        mytuple = asScreen, key, value_from_csv, 'value replace ok'
+
+                    # add new line
+                    elif value_from_csv and status == 'screen found, key not found':
+                        new_yaml = replaceOrAdd(
+                            screen_content, asScreen, key, value_from_csv, 'add')
+
+                        with open(r'new2.yaml', 'w') as write_yaml:
+                            write_yaml.writelines(new_yaml)
+
+                        mytuple = asScreen, key, value_from_csv, 'add key and value ok'
+
+                    print(filepath)
+                    print(mytuple)
+                    csv_writer.writerow(mytuple)
 
     return ''
+
+
 def print_help():
     print(main.__doc__)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
